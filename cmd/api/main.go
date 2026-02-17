6790ae62b86d3e3f060d5f783e832971bea5151f
Joseph Koop <koopjoseph7@gmail.com>
2026-02-16 20:52:27 -0600

    v3 complete

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,15 +21,6 @@ type applicationDependencies struct {
 	logger *slog.Logger
 }
 
-func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
-                                               r *http.Request) {
-    jsResponse := `{"status": "available", "environment": %q,
-                    "version": %q}`
-    jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
-    w.Header().Set("Content-Type", "application/json")
-    w.Write([]byte(jsResponse))
-} 
-
 func main() {
 	var settings serverConfig
 
