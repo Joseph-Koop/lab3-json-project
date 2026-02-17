4694d56bbb7a11e0eebe5aa7283047b7d5740a43
Joseph Koop <koopjoseph7@gmail.com>
2026-02-16 21:19:53 -0600

    Finished v-panic

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -63,12 +63,13 @@ func (a *applicationDependencies)methodNotAllowedResponse(
 
 func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
                                                r *http.Request) {
+//    panic("Apples & Oranges")   // deliberate panic
    data := envelope {
-                     "status": "available",
-                     "system_info": map[string]string{
-                             "environment": a.config.environment,
-                             "version": appVersion,
-                    },
+		"status": "available",
+		"system_info": map[string]string{
+				"environment": a.config.environment,
+				"version": appVersion,
+	},
    }
    err := a.writeJSON(w, http.StatusOK, data, nil)
    if err != nil {
