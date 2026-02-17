a843c686540963258f691359759498b2f856f7e5
Joseph Koop <koopjoseph7@gmail.com>
2026-02-16 21:01:49 -0600

    Finished v5

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,49 +1,50 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
+	"encoding/json"
+	"net/http"
 )
 
-func (a *applicationDependencies)writeJSON(w http.ResponseWriter,
-                                           status int, data any,
-                                           headers http.Header) error  {
-    jsResponse, err := json.MarshalIndent(data, "", "\t")
-    if err != nil {
-        return err
-    }
+type envelope map[string]any
 
-jsResponse = append(jsResponse, '\n')
-    // additional headers to be set
-    for key, value := range headers {
-        w.Header()[key] = value
-    }
-    // set content type header
-    w.Header().Set("Content-Type", "application/json")
-    // explicitly set the response status code
-    w.WriteHeader(status) 
-    _, err = w.Write(jsResponse)
-    if err != nil {
-        return err
-    }
+func (a *applicationDependencies) writeJSON(w http.ResponseWriter,
+	status int, data envelope,
+	headers http.Header) error {
+	jsResponse, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
 
-    return nil
+	jsResponse = append(jsResponse, '\n')
+	// additional headers to be set
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	// set content type header
+	w.Header().Set("Content-Type", "application/json")
+	// explicitly set the response status code
+	w.WriteHeader(status)
+	_, err = w.Write(jsResponse)
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
 
-
-func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
-                                               r *http.Request) {
-   data := map[string]string {
-                                "status": "available",
-                                "environment": a.config.environment,
-                                "version": appVersion,
-                              }
-   err := a.writeJSON(w, http.StatusOK, data, nil)
-   if err != nil {
-    a.logger.Error(err.Error())
-    http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-   }
+func (a *applicationDependencies) healthcheckHandler(w http.ResponseWriter,
+	r *http.Request) {
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": a.config.environment,
+			"version":     appVersion,
+		},
+	}
+	err := a.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		a.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
-
-
